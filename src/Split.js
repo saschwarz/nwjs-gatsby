057730830d09d36f848b62b5b7fc6a9d33eb1ae4
Steve Schwarz <steve@agilitynerd.com>
2019-11-17 11:39:55 -0600

    More content

--- src/Split.js
+++ src/Split.js
@@ -4,6 +4,8 @@ import styled from 'styled-components'
 const SplitLeftAlign = styled(Split)`
   text-align: left !important;
   align-items: start !important;
+  width: 70%;
+  padding: 0 2em;
 `
 
 export default SplitLeftAlign
